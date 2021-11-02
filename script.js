f90179bcc177023ba7468fd4b78a4a1fa2fceb2d
5steellix8 <metehankasapp@gmail.com>
2021-11-02 08:24:19 +0300

    main

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function progreset(id) {
     /* RESET OTHER CSS*/
     $('.progresInner').css('height', '0%');
     $('.liHeader').css('color', '#5f6368');
+    $(`.liImages ul li`).removeClass();
+    $(`.liImages ul li`).hide('500');
+
     /*LOOP FOR PROGRESS BAR */
+    $(`.liImages ul li:nth-child(${id})`).show('500');
     $(`#liHeader${id}`).css('color', 'var(--gmail-blue)')
     const progressWho = `progresInner${id}`
     document.getElementById(progressWho).style.height = `0%`;
