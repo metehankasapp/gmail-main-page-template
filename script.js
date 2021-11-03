1ea3976a65e9b0b6bec551c09c892ec35193d344
5steellix8 <metehankasapp@gmail.com>
2021-11-04 01:30:29 +0300

    mai

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+var x = window.matchMedia("(max-width: 992px)")
+
+
 function progreset(id) {
     /* RESET OTHER CSS*/
+    $('#progress1').css('background-color', 'rgba(0, 0, 0, 0.16)')
     $('.progresInner').css('height', '0%');
     $('.liHeader').css('color', '#5f6368');
     $(`.liImages ul li`).removeClass();
-    $(`.liImages ul li`).hide('500');
+    $(`.liImages ul li`).hide('100');
+
+    if (x.matches) {
+        $(`.ss3-ul:nth-child(1) img`).each(function (index) {
+            $($(`.ss3-ul:nth-child(1) img`)[index]).removeClass('d-inline-block');
+            $($(`.ss3-ul:nth-child(1) img`)[index]).addClass('d-none');
+
+        });
+        const curImg = $(`.ss3-ul:nth-child(1) img`)[id - 1];
+        $(curImg).removeClass('d-none');
+        $(curImg).addClass('d-inline-block');
+    } else {}
+
+
 
     /*LOOP FOR PROGRESS BAR */
-    $(`.liImages ul li:nth-child(${id})`).show('500');
+    $(`.liImages ul li:nth-child(${id})`).show('100');
     $(`#liHeader${id}`).css('color', 'var(--gmail-blue)')
     const progressWho = `progresInner${id}`
     document.getElementById(progressWho).style.height = `0%`;
